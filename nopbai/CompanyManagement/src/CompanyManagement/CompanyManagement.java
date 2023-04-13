345add1d0c281eb569682b2ad341e82f8b9901af
trinhquoc01 <115643074+trinhquoc01@users.noreply.github.com>
2023-04-14 00:19:47 +0700

    Add files via upload

---
+++ nopbai/CompanyManagement/src/CompanyManagement/CompanyManagement.java
@@ -0,0 +1,174 @@
+package CompanyManagement;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class CompanyManagement {
+    public static void main(String[] args) {
+        CompanyManagement.MainMenu();
+    }
+    private static List<Person> list = new ArrayList<Person>();
+    private static Person person;
+    public static void MainMenu() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("1. Add New Person");
+            System.out.println("2. Show all people");
+            System.out.println("3. Search people by id");
+            System.out.println("4. Update person information");
+            System.out.println("5. See the person's salary");
+            System.out.println("6. Update salary for people");
+            System.out.print("Enter your selection (1 - 6): ");
+            int selectedMenu = Integer.parseInt(scanner.nextLine());
+            switch (selectedMenu) {
+                case 1 -> {
+                    addnewperson();
+                }
+                case 2 -> {
+                    displayall();
+                }
+                case 3 -> {
+                    findPersonById();
+                }
+                case 4 -> {
+                    updatePerson();
+                }
+                case 5 -> {
+                    checkSalary();
+                }
+                case 6 -> {
+                    updateSalary();
+                }
+            }
+        }
+
+    }
+    private static void addnewperson() {
+        Scanner scanner = new Scanner(System.in);
+        int selectedMenu = 0;
+        addnewpersonLoop: while (true) {
+            System.out.println();
+            System.out.println("1. Director");
+            System.out.println("2. Manager");
+            System.out.println("3. Employee");
+            System.out.println("Enter Q to return");
+            try {
+                selectedMenu = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException numberFormatException) {
+                break addnewpersonLoop;
+            }
+            switch (selectedMenu) {
+                case 1 -> {
+                    person = new Director();
+                }
+                case 2 -> {
+                    person = new Manager();
+                }
+                case 3 -> {
+                    person = new Employee();
+                }
+            }
+            person.input();
+            list.add(person);
+        }
+    }
+    private static void displayall() {
+        System.out.println("Show total number of people");
+        for (Person person : list) {
+            person.display();
+        }
+    }
+    private static void findPersonById() {
+         Scanner scanner = new Scanner(System.in);
+         while (true) {
+            System.out.println();
+            int id;
+            System.out.print("Enter the ID of the person you want to search : ");
+            try {
+                id = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException numberFormatException) {
+                break ;
+            }
+            Person result = findPerson(id);
+            if (result != null) {
+                System.out.print("Results found : ");
+                result.display();
+            } else {
+                System.out.println("Can't find anyone");
+            }
+        }
+    }
+    private static Person findPerson(int id) {
+        for (Person person : list) {
+            if (person.getId() == id) {
+                return person;
+            }
+        }
+        return null;
+    }
+    private static void updatePerson() {
+        Scanner scanner = new Scanner(System.in);
+        findPersonLoop: while (true) {
+            System.out.println();
+            int id;
+            System.out.print("Enter the ID of the person you want to update : ");
+            try {
+                id = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException numberFormatException) {
+                break findPersonLoop;
+            }
+            Person result = findPerson(id);
+            if (result != null) {
+                System.out.print("Results found: ");
+                result.display();
+                result.updatePerson();
+            } else {
+                System.out.println("Can't find anyone");
+            }
+        }
+    }
+    private static void checkSalary() {
+        Scanner scanner = new Scanner(System.in);
+        findPersonLoop: while (true) {
+            System.out.println();
+            int id;
+            System.out.print("Enter the ID of the person you want to see the amount of : ");
+            try {
+                id = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException numberFormatException) {
+                break findPersonLoop;
+            }
+            Person result = findPerson(id);
+            if (result != null) {
+                System.out.print("Results found: ");
+                result.display();
+            } else {
+                System.out.println("Can't find anyone");
+            }
+        }
+    }
+    private static void updateSalary() {
+        Scanner scanner = new Scanner(System.in);
+        findPersonLoop: while (true) {
+            System.out.println();
+            int id;
+            System.out.print("Enter the ID of the person you want to update : ");
+            try {
+                id = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException numberFormatException) {
+                break findPersonLoop;
+            }
+            Person result = findPerson(id);
+            if (result != null) {
+                System.out.print("Results found: ");
+                result.display();
+                result.updateSalary();
+            } else {
+                System.out.println("Can't find anyone");
+            }
+        }
+    }
+
+}
