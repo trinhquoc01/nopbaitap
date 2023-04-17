de01cb3953ec27045e3456d8eb1a72fd1f14b8f2
trinhquoc01 <115643074+trinhquoc01@users.noreply.github.com>
2023-04-17 10:59:40 +0700

    Update MainAccount.java

--- nopbai/exam-jp1/src/examjp1/MainAccount.java
+++ nopbai/exam-jp1/src/examjp1/MainAccount.java
@@ -11,7 +11,7 @@ public class MainAccount {
         account.input();
         System.out.print("Deposit (0) or Withdraw (1): ");
         int type = Integer.parseInt(scanner.nextLine());
-        System.out.print("Enter amount of money ($): ");
+        System.out.print("Enter amount of money : ");
         long amount = Integer.parseInt(scanner.nextLine());
         account.depositAndWithdraw(amount, type);
         System.out.println(account);
