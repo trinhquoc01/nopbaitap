2a248ff7ca957a261a76f3150a4d32b841a03cc0
trinhquoc01 <115643074+trinhquoc01@users.noreply.github.com>
2023-07-16 21:13:55 +0700

    Add files via upload

---
+++ thietke/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppADONET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentController studentController = new StudentController();
+            //studentController.GetAllStudents();
+
+            //Student student = new Student { Name = "Tom", Email = "tom@gmail.com", Level = 2 };
+            //studentController.CreateNewStudent(student);
+
+            //Get Student from DB by ID = 2
+
+            Student student 
+                = new Student { Id = 2, Name = "Tom Cruise", Email = "tomCruise@gmail.com", Level = 2 };
+            studentController.UpdateStudent(student);
+
+            Console.WriteLine("================================");
+            studentController.GetAllStudents();
+
+        }
+    }
+}
