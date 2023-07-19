fc51e5152343ce66130fcb640207d2f8760aced9
trinhquoc01 <115643074+trinhquoc01@users.noreply.github.com>
2023-07-19 11:06:33 +0700

    Add files via upload

---
+++ nopbai/exam/Controller/ProductController.cs
@@ -0,0 +1,75 @@
+﻿using exam.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam.Controller
+{
+    internal class ProductController
+    {
+        private List<Product> products = new List<Product>();
+
+        public void AddProduct() 
+        {
+            Console.WriteLine("---- Add Prodcut ----");
+            Console.Write("Enter Product Id : ");
+            string productId = Console.ReadLine();
+
+            Console.Write("Enter Product Name : ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Product Price :$");
+            double price = int.Parse(Console.ReadLine());
+
+            Product product = new Product
+            {
+                ProductId = productId,
+                Name = name,
+                Price = price
+            };
+
+            products.Add(product);
+            Console.WriteLine(" Add successfully ");
+
+        }
+
+        public void DisplayAllProduct() 
+        {
+            Console.WriteLine("---- All Products ----");
+            if(products.Count == 0)
+            {
+                Console.WriteLine(" No Products ");
+            }
+            else
+            {
+                foreach (var product in products) 
+                {
+                    Console.WriteLine($"ProductId               " + "Product Name               " + "Price");
+                    Console.WriteLine($"{product.ProductId}                      " + $"{product.Name}                  " + $"${product.Price}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void DeleteProductById()
+        {
+            Console.WriteLine("---- Delete Product By Id ----"); 
+            Console.Write("Enter ProductId : ");
+            string productId = Console.ReadLine();
+
+            Product productRemove = products.Find(p => p.ProductId == productId);
+
+            if (productRemove != null)
+            {
+                products.Remove(productRemove);
+                Console.WriteLine("Delete successfully");
+            }
+            else 
+            {
+                Console.WriteLine("Product not found");
+            }
+        }
+    }
+}
---
+++ nopbai/exam/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using exam.Controller;
+using System;
+
+ProductController productController = new ProductController();
+
+int choose = 0;
+
+try
+{
+    do
+    {
+        Console.WriteLine("---- Product Management Menu ----");
+        Console.WriteLine("1.Add Product");
+        Console.WriteLine("2.Display all products");
+        Console.WriteLine("3.Delete product by Id");
+        Console.WriteLine("4.Exit");
+
+        choose = int.Parse(Console.ReadLine());
+
+        switch (choose)
+        {
+            case 1:
+
+                productController.AddProduct();
+                break;
+            case 2:
+                productController.DisplayAllProduct();
+                break;
+            case 3:
+                productController.DeleteProductById();
+                break;
+            case 4:
+                return;
+        }
+    } while (true);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+} 
+
+
